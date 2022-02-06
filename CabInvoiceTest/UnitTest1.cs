23a5431257eeee765f48479487231e401074ae61
Lenovo <somusp75@gmail.com>
2022-02-06 20:26:36 +0530

    step-2 Multiple Rides

--- CabInvoiceTest/UnitTest1.cs
+++ CabInvoiceTest/UnitTest1.cs
@@ -9,6 +9,7 @@ namespace CabInvoiceTest
         InvoiceGenerator invoiceGenerator = null;
 
         [TestMethod]
+        //step-1 test case
         public void GivenDistanceAndTime_ShouldReturnTotalFare()
         {
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
@@ -28,6 +29,19 @@ namespace CabInvoiceTest
             double fare = invoiceGenerator.CalculateFare(distance, time);
             double expected = 5;
             Assert.AreEqual(expected, fare);
+        }
+        [TestMethod]
+        //step-2 test case 
+        public void GivenMultipleRides_ShouldReturnTotalFare()
+        {
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1)
+            };
+            double fare = invoiceGenerator.CalculateFare(rides);
+            double expected = 30;
+            Assert.AreEqual(expected, fare);
+
         }
 
     }
